2b6f2f2d820f2c03cc9c40e30575777c68b65c50
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-07-13 01:04:13 +0600

    added checkboxes

--- app/src/components/data/poems.js
+++ app/src/components/data/poems.js
@@ -1,13 +1,63 @@
 export const poems = [
 {
-    name: "hello",
-    text: "world",
-    author: "slim shady"
-},
-{
     name: "Білімдіден шыққан сөз",
     text: `Білімдіден шыққан сөз\nТалаптыға болсын кез.\nНұрын, сырын көруге,\nКөкірегінде болсын көз.`,
-    author: "Абай Құнанбаев"
+    author: "Абай",
+    number: 1,
+},
+{
+    name: "Пәк қыз",
+    text: "Қараңғы, тыныштық өңгерген,\nЕкеуміз мекендеген кеш.\nСен шықтың арғылау бөлмеден,\nБұл жолы киімен емес…",
+    author: "Мақатаев",
+    number: 2
+},
+{
+    name: "test1",
+    text: "",
+    author: "test",
+    number: 3
+},
+{
+    name: "test2",
+    text: "",
+    author: "test",
+    number: 4
+},
+{
+    name: "test3",
+    text: "",
+    author: "test",
+    number: 5
+},
+{
+    name: "test4",
+    text: "",
+    author: "test",
+    number: 6
+},
+{
+    name: "",
+    text: "",
+    author: "",
+    number: 7
+},
+{
+    name: "",
+    text: "",
+    author: "",
+    number: 8
+},
+{
+    name: "",
+    text: "",
+    author: "",
+    number: 9
+},
+{
+    name: "",
+    text: "",
+    author: "",
+    number: 10
 },
 ]
 
